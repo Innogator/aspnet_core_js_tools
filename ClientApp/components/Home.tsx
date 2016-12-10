7136921d871a58581bd480ffd75b00962bc7ad51
btanne6 <btanne6@gmail.com>
2016-12-10 18:49:20 -0500

    CI Test

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -4,7 +4,7 @@ export default class Home extends React.Component<void, void> {
     public render() {
         return <div>
             <h1>Hello, world!</h1>
-            <p>Welcome to your new single-page application, built with:</p>
+            <p>Welcome to your new single-page application built with:</p>
             <ul>
                 <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
                 <li><a href='https://facebook.github.io/react/'>React</a>, <a href='http://redux.js.org'>Redux</a>, and <a href='http://www.typescriptlang.org/'>TypeScript</a> for client-side code</li>

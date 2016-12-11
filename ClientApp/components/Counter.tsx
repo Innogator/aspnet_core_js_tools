79ae571ce26e1cd392c94c1e019ec267d593c836
btanne6 <btanne6@gmail.com>
2016-12-11 16:04:53 -0500

    added decrement

--- ClientApp/components/Counter.tsx
+++ ClientApp/components/Counter.tsx
@@ -17,6 +17,7 @@ class Counter extends React.Component<CounterProps, void> {
             <p>Current count: <strong>{ this.props.count }</strong></p>
 
             <button onClick={ () => { this.props.increment() } }>Increment</button>
+            <button onClick={ () => { this.props.decrement() } }>Decrement</button>
         </div>;
     }
 }
